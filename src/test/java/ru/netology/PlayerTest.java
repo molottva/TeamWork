a980af3d0fe44e5589e5f71bb9db2566017fd94f
Nikita Santalov <na.santalov@yandex.ru>
2022-05-21 23:25:10 +0400

    addtests

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -19,5 +20,82 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+
+    @Test
+    public void TestSumGenreTwoGames() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Ведьмак", "Рпг");
+        Game game1 = store.publishGame("Ведьмак 2", "Рпг");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.installGame(game3);
+        player.play(game, 3);
+        player.play(game1, 2);
+        player.play(game3, 1);
+
+        int expected = 5;
+        int actual = player.sumGenre("Рпг");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowRunTimeException() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Ведьмак", "Рпг");
+        Game game1 = store.publishGame("Ведьмак 2", "Рпг");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+
+
+        assertThrows(RuntimeException.class, () -> {
+            player.play(game3, 1);
+
+        });
+    }
+
+    @Test
+    public void Test3() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Ведьмак", "Рпг");
+        Game game1 = store.publishGame("Ведьмак 2", "Рпг");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.installGame(game3);
+        player.play(game, 3);
+        player.play(game1, 2);
+        player.play(game3, 1);
+
+        String expected = game.getTitle();
+        Game actual = player.mostPlayerByGenre("Рпг");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMostPlayerByGenre() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Ведьмак", "Рпг");
+        Game game1 = store.publishGame("Ведьмак 2", "Рпг");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 3);
+        player.play(game1, 2);
+
+
+        String expected = null;
+        Game actual = player.mostPlayerByGenre("Аркады");
+        assertEquals(expected, actual);
+    }
+
+
 }
