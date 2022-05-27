d4ea20c5aa8d3f5f6098ca4ad6c1f9dcf09f138f
molottva <molottva@list.ru>
2022-05-27 17:28:45 +0300

    fixes #9

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -26,7 +26,9 @@ public class Player {
      * если игра уже была, никаких изменений происходить не должно
      */
     public void installGame(Game game) {
-        playedTime.put(game, 0);
+        if (!playedTime.containsKey(game)) {
+            playedTime.put(game, 0);
+        }
     }
 
     /**
